419e9c6c82667b94587be77790fd3eb3c15f9363
arun521414 <arunayangudi36@gmail.com>
2025-01-04 01:33:52 +0530

    first commit

---
+++ vannam_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VannamAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vannam_auth'
---
+++ vannam_user/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VannamUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vannam_user'
